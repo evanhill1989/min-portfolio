0aad3837ee69a30d6940004c732aedb3a474ded0
Evan Hill <evanhill1989@gmail.com>
2025-09-14 17:45:25 -0400

    NavMenu

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
 export default function Home() {
   return (
     <>
-      <h1>this is what we should return</h1>
+      <h1>Evan Hill</h1>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink>Projects</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>Resume</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>Contact</NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </>
   );
 }
