804091b5f597c72a74a81030c343ba327f2f4593
EvanReact <evanhill1989@gmail.com>
2025-09-15 12:37:43 -0400

    Landing content

--- app/page.tsx
+++ app/page.tsx
@@ -8,32 +8,52 @@ import {
 export default function Home() {
   return (
     <>
-      <main className="">
+      <div className="wrapper">
         <div className="content-wrapper">
           <div className="content">
-            <div className="flex flex-col">
-              <h1 className="">Evan Hill</h1>
-              <p>Web Developer</p>
-            </div>
+            <header className="header flex flex-col gap-12 ">
+              <div className="flex flex-col">
+                <h1 className="text-5xl ml-[-4px] mt-[-0.175em] leading-none">
+                  Evan Hill
+                </h1>
+                <p>Web Developer</p>
+              </div>
 
-            <nav>
-              <NavigationMenu>
-                <NavigationMenuList className="flex-col gap-0">
-                  <NavigationMenuItem>
-                    <NavigationMenuLink>Projects</NavigationMenuLink>
-                  </NavigationMenuItem>
-                  <NavigationMenuItem>
-                    <NavigationMenuLink>Resume</NavigationMenuLink>
-                  </NavigationMenuItem>
-                  <NavigationMenuItem>
-                    <NavigationMenuLink>Contact</NavigationMenuLink>
-                  </NavigationMenuItem>
-                </NavigationMenuList>
-              </NavigationMenu>
-            </nav>
+              <nav className="nav">
+                <NavigationMenu>
+                  <NavigationMenuList className="flex-col font-bold">
+                    <NavigationMenuItem>
+                      <NavigationMenuLink>Projects</NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                      <NavigationMenuLink>Resume</NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                      <NavigationMenuLink>Contact</NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                      <NavigationMenuLink>Blog</NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                      <NavigationMenuLink>Favorite Websites</NavigationMenuLink>
+                    </NavigationMenuItem>
+                  </NavigationMenuList>
+                </NavigationMenu>
+              </nav>
+            </header>
+            <main className="main"></main>
+            <aside className="aside font-bold">
+              <p>
+                I am a<br /> full stack developer
+                <br /> based in Florida.
+                <br /> I solve problems.
+                <br /> I obsess about
+                <br /> strategic development & design.
+              </p>
+            </aside>
           </div>
         </div>
-      </main>
+      </div>
     </>
   );
 }
