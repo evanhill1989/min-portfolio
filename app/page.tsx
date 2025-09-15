171f88f13f994d02d978df2cfb5f2564143961cb
Evan Hill <evanhill1989@gmail.com>
2025-09-15 10:46:13 -0400

    switch to laptop

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,32 @@ import {
 export default function Home() {
   return (
     <>
-      <h1>Evan Hill</h1>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuLink>Projects</NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink>Resume</NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink>Contact</NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <main className="">
+        <div className="content-wrapper">
+          <div className="content">
+            <div className="flex flex-col">
+              <h1 className="">Evan Hill</h1>
+              <p>Web Developer</p>
+            </div>
+
+            <nav>
+              <NavigationMenu>
+                <NavigationMenuList className="flex-col gap-0">
+                  <NavigationMenuItem>
+                    <NavigationMenuLink>Projects</NavigationMenuLink>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <NavigationMenuLink>Resume</NavigationMenuLink>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <NavigationMenuLink>Contact</NavigationMenuLink>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </nav>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
